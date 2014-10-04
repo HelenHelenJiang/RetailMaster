6b64a43a082370d49637535c3307afe19ca1e052
Hailun Jiang <helenapplication@gmail.com>
2014-10-04 11:22:22 -0400

    collection view

---
+++ RetailMaster/Controllers/ItemViewController.h
@@ -0,0 +1,22 @@
+//
+//  ItemViewController.h
+//  RetailMasterHack
+//
+//  Created by Helen Jiang on 2014-10-04.
+//  Copyright (c) 2014 RetailMaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ItemViewController : UICollectionViewController<UICollectionViewDelegate, UICollectionViewDelegate>
+{
+    NSArray *ImageNames;
+    NSArray *NameLabels;
+    NSArray *PriceLabels;
+    
+}
+
+
+
+
+@end
---
+++ RetailMaster/Views/CollectionViewCell.m
@@ -0,0 +1,31 @@
+//
+//  CollectionViewCell.m
+//  RetailMasterHack
+//
+//  Created by Helen Jiang on 2014-10-04.
+//  Copyright (c) 2014 RetailMaster. All rights reserved.
+//
+
+#import "CollectionViewCell.h"
+
+@implementation CollectionViewCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+ // Only override drawRect: if you perform custom drawing.
+ // An empty implementation adversely affects performance during animation.
+ - (void)drawRect:(CGRect)rect
+ {
+ // Drawing code
+ }
+ */
+
+@end
