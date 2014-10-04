3964b31a47bd648d460c0ccbedb2292106bcb3ac
CarlZhou <wuxi8black@gmail.com>
2014-10-04 19:08:09 -0400

    update cat view

--- RetailMaster/Controllers/CatagoriesViewController.h
+++ RetailMaster/Controllers/CatagoriesViewController.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface CatagoriesViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UICollectionView *collectionView;
 
 @end
--- RetailMaster/Controllers/CatagoriesViewController.m
+++ RetailMaster/Controllers/CatagoriesViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "CatagoriesViewController.h"
+#import "CategoryCollectionViewCell.h"
 
-@interface CatagoriesViewController ()
+@interface CatagoriesViewController ()<UICollectionViewDataSource, UICollectionViewDelegate>
 {
     NSArray *CategoryNames;
     NSArray *CategoryImages;
@@ -37,6 +38,23 @@
     // Do any additional setup after loading the view from its nib.
 }
 
+- (void)initCollectionView
+{
+    UICollectionViewFlowLayout *flowlayout = [[UICollectionViewFlowLayout alloc] init];
+    [flowlayout setItemSize:CGSizeMake(self.collectionView.bounds.size.width, 500)];
+    //    [flowlayout setHeaderReferenceSize:CGSizeMake(320, 210)];
+    
+    [self.collectionView setCollectionViewLayout:flowlayout];
+    self.collectionView.delegate = self;
+    self.collectionView.dataSource = self;
+    self.collectionView.backgroundColor = [UIColor whiteColor];
+    [self.collectionView registerClass:[CategoryCollectionViewCell class] forCellWithReuseIdentifier:@"CategoryCollectionViewCell"];
+    //    UINib *headerNib = [UINib nibWithNibName:@"ProfileCollectionHeader" bundle:nil];
+    //    [self.collectionView registerNib:headerNib forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"HeaderView"];
+//    UINib *headerNib = [UINib nibWithNibName:@"UploadingSnapHeader" bundle:nil];
+//    [self.collectionView registerNib:headerNib forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"UploadingSnapHeader"];
+}
+
 #pragma mark collectionView delegates
 - (NSInteger) numberOfSectionsInCollectionView:(UICollectionView *)collectionView
 {
@@ -45,36 +63,38 @@
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
-    return self.snapsData.count;
+    return CategoryNames.count;
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
 {
-    NSString *kIdentifier = @"FoodSnapCollectionCell";
+    NSString *kIdentifier = @"CategoryCollectionViewCell";
     
-    FoodSnapCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:kIdentifier forIndexPath:indexPath];
+    CategoryCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:kIdentifier forIndexPath:indexPath];
+//
+//    kFoodSnap *snap = self.snapsData[indexPath.row];
+//    
+//    cell.foodSnapCellMainImageView.image = snap.kFoodSnapImage;
+//    [cell.foodSnapCellUserImageView sd_setImageWithURL:[NSURL URLWithString:snap.kFoodSnapUserAvatarURL]];
+//    cell.foodSnapCellUserNameLabel.text = snap.kFoodSnapUsername;
+//    cell.foodSnapCellTimeLabel.text = [snap.kFoodSnapCreatedDate prettyDate];
+//    cell.foodSnapPriceLabel.text = [NSString stringWithFormat:@"$%.2f", [snap.kFoodSnapPrice doubleValue]];
+//    cell.cellSnap = snap;
+//    [cell setCommentCountLabelWithText:[NSString stringWithFormat:@"%@ comments", snap.kFoodSnapCommentCount]];
+//    if (snap.isLiked)
+//    {
+//        cell.actionButtonContainer.hidden = YES;
+//        cell.percentageContainer.hidden = NO;
+//        [cell setPercentageLabels];
+//    }
+//    else
+//    {
+//        cell.actionButtonContainer.hidden = NO;
+//        cell.percentageContainer.hidden = YES;
+//    }
+//    cell.delegate = self;
     
-    kFoodSnap *snap = self.snapsData[indexPath.row];
-    
-    cell.foodSnapCellMainImageView.image = snap.kFoodSnapImage;
-    [cell.foodSnapCellUserImageView sd_setImageWithURL:[NSURL URLWithString:snap.kFoodSnapUserAvatarURL]];
-    cell.foodSnapCellUserNameLabel.text = snap.kFoodSnapUsername;
-    cell.foodSnapCellTimeLabel.text = [snap.kFoodSnapCreatedDate prettyDate];
-    cell.foodSnapPriceLabel.text = [NSString stringWithFormat:@"$%.2f", [snap.kFoodSnapPrice doubleValue]];
-    cell.cellSnap = snap;
-    [cell setCommentCountLabelWithText:[NSString stringWithFormat:@"%@ comments", snap.kFoodSnapCommentCount]];
-    if (snap.isLiked)
-    {
-        cell.actionButtonContainer.hidden = YES;
-        cell.percentageContainer.hidden = NO;
-        [cell setPercentageLabels];
-    }
-    else
-    {
-        cell.actionButtonContainer.hidden = NO;
-        cell.percentageContainer.hidden = YES;
-    }
-    cell.delegate = self;
+    cell.catImageView.image = [UIImage imageNamed:CategoryImages[indexPath.row]];
     
     return cell;
 }
@@ -102,23 +122,23 @@
     
     if (kind == UICollectionElementKindSectionHeader)
     {
-        UploadingSnapHeader *headerView = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"UploadingSnapHeader" forIndexPath:indexPath];
-        kFoodSnap *snap = [ParseUtil sharedUtil].uploadingSnap;
-        headerView.imageView.image = snap.kFoodSnapImage;
-        if (snap.isUploadingFailed)
-        {
-            headerView.retryButton.hidden = NO;
-            headerView.cancelButton.hidden = NO;
-            headerView.statusLabel.text = @"Failed";
-        }
-        else
-        {
-            headerView.retryButton.hidden = YES;
-            headerView.cancelButton.hidden = YES;
-            headerView.statusLabel.text = @"Uploading";
-        }
-        currentUploadingHeaderView = headerView;
-        reusableview = currentUploadingHeaderView;
+//        UploadingSnapHeader *headerView = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"UploadingSnapHeader" forIndexPath:indexPath];
+//        kFoodSnap *snap = [ParseUtil sharedUtil].uploadingSnap;
+//        headerView.imageView.image = snap.kFoodSnapImage;
+//        if (snap.isUploadingFailed)
+//        {
+//            headerView.retryButton.hidden = NO;
+//            headerView.cancelButton.hidden = NO;
+//            headerView.statusLabel.text = @"Failed";
+//        }
+//        else
+//        {
+//            headerView.retryButton.hidden = YES;
+//            headerView.cancelButton.hidden = YES;
+//            headerView.statusLabel.text = @"Uploading";
+//        }
+//        currentUploadingHeaderView = headerView;
+//        reusableview = currentUploadingHeaderView;
     }
     
     return reusableview;
@@ -131,23 +151,25 @@
 
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout referenceSizeForHeaderInSection:(NSInteger)section
 {
-    if ([ParseUtil sharedUtil].uploadingSnap && section == 0)
-    {
-        return CGSizeMake(self.collectionView.bounds.size.width, 44);
-    }
-    else
-    {
-        return CGSizeMake(0, 0);
-    }
+    return CGSizeMake(self.collectionView.bounds.size.width, 44);
+    
+//    if ([ParseUtil sharedUtil].uploadingSnap && section == 0)
+//    {
+//        return CGSizeMake(self.collectionView.bounds.size.width, 44);
+//    }
+//    else
+//    {
+//        return CGSizeMake(0, 0);
+//    }
 }
 
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
 {
-    kFoodSnap *item = self.snapsData[indexPath.row];
-    
-    [SSPhotoViewer showPhotoViewerWithSnap:item To:[UIUtil getMainDrawerController].view];
-    
-    [collectionView deselectItemAtIndexPath:indexPath animated:YES];
+//    kFoodSnap *item = self.snapsData[indexPath.row];
+//    
+//    [SSPhotoViewer showPhotoViewerWithSnap:item To:[UIUtil getMainDrawerController].view];
+//    
+//    [collectionView deselectItemAtIndexPath:indexPath animated:YES];
 }
 
 @end
