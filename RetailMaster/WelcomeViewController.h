4b44e758718664231d2169a3839b0dd33bdcea07
Ri Xu <rixu.jack@gmail.com>
2014-10-04 13:37:53 -0400

    WelcomeVC initial

---
+++ RetailMaster/WelcomeViewController.h
@@ -0,0 +1,15 @@
+//
+//  WelcomeViewController.h
+//  RetailMaster
+//
+//  Created by Jack on 2014-10-04.
+//  Copyright (c) 2014 CZLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WelcomeViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIButton *button;
+
+@end
