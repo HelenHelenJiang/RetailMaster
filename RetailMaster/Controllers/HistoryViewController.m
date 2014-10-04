a4d965549800811095e915f5e7947316f93e0f54
CarlZhou <wuxi8black@gmail.com>
2014-10-04 15:32:35 -0400

    init history view controller

---
+++ RetailMaster/Controllers/HistoryViewController.m
@@ -0,0 +1,49 @@
+//
+//  HistoryViewController.m
+//  RetailMaster
+//
+//  Created by carlzhou on 10/4/14.
+//  Copyright (c) 2014 CZLabs. All rights reserved.
+//
+
+#import "HistoryViewController.h"
+
+@interface HistoryViewController ()
+
+@end
+
+@implementation HistoryViewController
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
