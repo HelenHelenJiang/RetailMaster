a1e30c40a3405782e2196f085ec30b4ee4c93b93
CarlZhou <wuxi8black@gmail.com>
2014-10-04 12:51:19 -0400

    update checkout vc

---
+++ RetailMaster/Views/PickupTimeTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  PickupTimeTableViewCell.h
+//  RetailMaster
+//
+//  Created by carlzhou on 10/4/14.
+//  Copyright (c) 2014 CZLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PickupTimeTableViewCell : UITableViewCell
+
+@property (strong, nonatomic) IBOutlet UILabel *fromTimeLabel;
+@property (strong, nonatomic) IBOutlet UILabel *toTimeLabel;
+
+@end
