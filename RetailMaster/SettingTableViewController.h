6830f6e4a6e1608ad08d8a363cb5df4943465518
Ri Xu <rixu.jack@gmail.com>
2014-10-05 07:26:09 -0400

    fix bugs on some view controllers

---
+++ RetailMaster/SettingTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingTableViewController.h
+//  RetailMaster
+//
+//  Created by Jack on 2014-10-05.
+//  Copyright (c) 2014 CZLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingTableViewController : UITableViewController
+
+@end
